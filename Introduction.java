794b2e5875bf2f89395cb91ebd8071d6adea448e
NosDrg <long2ktb@gmail.com>
2025-09-21 22:57:12 +0700

    24020205 HW1-B

---
+++ Introduction.java
@@ -0,0 +1,8 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyen Cao Bao Long\t24020206\tK69I-IT1\tNosDrg\tlong2k6tb@gmail.com");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n\nTake one down, pass it around,\n\n8 bottles of beer on the wall, 8 bottles of beer.\n\nTake one down, pass it around,\n\n7 bottles of beer on the wall, 7 bottles of beer.\n\nTake one down, pass it around,\n\n6 bottles of beer on the wall, 6 bottles of beer.\n\nTake one down, pass it around,\n\n5 bottles of beer on the wall, 5 bottles of beer.\n\nTake one down, pass it around,\n\n4 bottles of beer on the wall, 4 bottles of beer.\n\nTake one down, pass it around,\n\n3 bottles of beer on the wall, 3 bottles of beer.\n\nTake one down, pass it around,\n\n2 bottles of beer on the wall, 2 bottles of beer.\n\nTake one down, pass it around,\n\n1 bottle of beer on the wall, 1 bottle of beer.\n\nTake one down, pass it around,\n\nNo more bottles of beer on the wall.");
+        
+    }
+}
+
